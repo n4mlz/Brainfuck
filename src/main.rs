a6058f946bdb108196e7dd5e8aeac3b16fb9d734
n4mlz <n4mlz.dev@gmail.com>
2025-08-08 17:01:14 +0900

    :zap: Support both the interpreter and the compiler

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 use std::{env, fs, io, process};
 
-use crate::interpreter::Interpreter;
-
+mod codegen;
 mod interpreter;
 mod lexer;
 mod parser;
 
+fn usage(prog: &str) -> ! {
+    eprintln!("Usage: {prog} (compile|c|interpret|i) <source.bf>");
+    process::exit(1);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <path>", args[0]);
-        process::exit(1);
-    }
 
-    let file_path = &args[1];
-    let contents = fs::read_to_string(file_path).unwrap_or_else(|err| {
-        eprintln!("Failed to read file: {file_path}: {err}");
+    let prog = args.first().unwrap();
+    let cmd = args.get(1).unwrap_or_else(|| usage(prog));
+    let path = args.get(2).unwrap_or_else(|| usage(prog));
+
+    let contents = fs::read_to_string(path).unwrap_or_else(|err| {
+        eprintln!("Failed to read file: {path}: {err}");
         process::exit(1);
     });
 
     let tokens = lexer::lex(&contents);
     let nodes = parser::parse(&tokens);
 
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let mut interpreter = Interpreter::new(stdin, stdout);
-
-    interpreter.run(&nodes).unwrap();
+    match cmd.as_str() {
+        "compile" | "c" => {
+            let ir = codegen::generate_ir(&nodes);
+            println!("{ir}");
+        }
+        "interpret" | "i" => {
+            let mut interpreter = interpreter::Interpreter::new(io::stdin(), io::stdout());
+            interpreter.run(&nodes).unwrap();
+        }
+        _ => usage(prog),
+    }
 }
